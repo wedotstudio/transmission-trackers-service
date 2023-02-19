01feec48ee112249a190b701de195046b8c95a69
Jinming Wu, Patrick <me@patrickwu.space>
2023-02-20 01:02:31 +0800

    Initial files completion

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # transmission-trackers-service
-A automatic tracker addition service for Transmission 4.0
+An automatic tracker addition service for Transmission 4.0 on Linux. this is inspired by [transmission-trackers](https://github.com/blind-oracle/transmission-trackers). This project is utilizing the new version of Transmission feature which allows you to add default trackers in the settings.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
---
+++ transmission-trackers-service.py
@@ -0,0 +1,100 @@
+import json, os, re, subprocess
+from shutil import copyfile
+from argparse import ArgumentParser
+from distutils.spawn import find_executable
+from urllib import request
+
+from packaging.version import parse
+
+# check if transmission is installed
+
+if find_executable("transmission-gtk") is None:
+    print("Transmission is not installed")
+    exit(1)
+
+# check if transmission is higher or equal to version 4.0.0
+result = subprocess.run(['transmission-gtk', '-v'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+result_str = result.stdout.decode('utf-8')
+version = re.findall(r'\d\.\d\.\d', str(result_str))
+if version is None:
+    print("Transmission seems to be installed but version could not be found")
+    exit(1)
+
+if parse(version[0]) < parse('4.0.0'):
+    print("Transmission version is too low")
+    exit(1)
+    
+argparser = ArgumentParser(description='Transmission tracker updater')
+argparser.add_argument('-c', '--config', help='Path to transmission config file')
+argparser.add_argument('-d', '--dry-run', help='Do not write to config file', action='store_true')
+argparser.add_argument('-f', '--file', help='Path to file with trackers')
+argparser.add_argument('--trackers', help='List of trackers to use', nargs='+')
+args = argparser.parse_args()
+
+if args.config:
+    config_path = args.config
+else:
+    home_directory = os.path.expanduser( '~' )
+    config_path = os.path.join(home_directory, '.config', 'transmission', 'settings.json')
+
+trackers_list = []
+default_tracker = "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt"
+
+if args.file:
+    try:
+        with open(args.file, 'r') as file:
+            for line in file:
+                trackers_list.append(line.strip())
+    except:
+        print("File could not be opened, skipped")
+if args.trackers:
+    trackers_list = args.trackers
+
+if not trackers_list:
+    trackers_list.append(default_tracker)
+
+text = ""
+
+for tracker in trackers_list:
+    response = request.urlopen(tracker)
+    data = response.read()
+    raw_text = data.decode('utf-8')
+    for line in raw_text.splitlines():
+        if not line or line.startswith('#') or line.startswith('ws'):
+            continue
+        text += line + "\n"
+text = text[:-1]
+
+if args.dry_run:
+    print(text)
+    exit(0)
+
+# check if config file exists
+if not os.path.isfile(config_path):
+    print("Config file does not exist")
+    exit(1)
+
+# backup config file
+backup_path = config_path + ".bak"
+try:
+    copyfile(config_path, backup_path)
+except:
+    print("Could not backup config file")
+    exit(1)
+
+# modify config file
+try:
+    with open(config_path, 'r') as file:
+        data = json.load(file)
+        data['default-trackers'] = text
+except:
+    print("Could not load config file")
+    exit(1)
+    
+try:
+    with open(config_path, 'w') as file:
+        json.dump(data, file, indent=4)
+except:
+    print("Could not write to config file")
+    exit(1)
+
